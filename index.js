8fadc269843a1a9a75f1f046ce6bbec6876a7c20
feryyinfotech <101308921+feryyinfotech@users.noreply.github.com>
2024-05-26 09:26:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -240,37 +240,37 @@ if (timeToSend === 6) {
       const res = await axios.get(
         `https://apilist.tronscanapi.com/api/block?sort=-balance&start=0&limit=20&producer=&number=&start_timestamp=${datetoAPISend}&end_timestamp=${datetoAPISend}`
       );
-      if (res?.data?.data[0]) {
-        const obj = res.data.data[0];
-        const fd = new FormData();
-        fd.append("hash", `**${obj.hash.slice(-4)}`);
-        fd.append("digits", `${obj.hash.slice(-5)}`);
-        fd.append("number", obj.number);
-        fd.append("time", moment(time).format("HH:mm:ss"));
-        let prevalue = `${moment(time).format("HH:mm:ss")}`;
-        const newString = obj.hash;
-        let num = null;
-        for (let i = newString.length - 1; i >= 0; i--) {
-          if (!isNaN(parseInt(newString[i]))) {
-            num = parseInt(newString[i]);
-            break;
-          }
-        }
-        fd.append("slotid", num);
-        fd.append("overall", JSON.stringify(obj));
-        //  trx 1
-        try {
-           if (String(isAlreadyHit) === String(prevalue)) return;
-          const response = await axios.post(
-            "https://admin.zupeeter.com/Apitrx/insert_one_trx",
-            fd
-          );
-              isAlreadyHit = prevalue;
+      // if (res?.data?.data[0]) {
+      //   const obj = res.data.data[0];
+      //   const fd = new FormData();
+      //   fd.append("hash", `**${obj.hash.slice(-4)}`);
+      //   fd.append("digits", `${obj.hash.slice(-5)}`);
+      //   fd.append("number", obj.number);
+      //   fd.append("time", moment(time).format("HH:mm:ss"));
+      //   let prevalue = `${moment(time).format("HH:mm:ss")}`;
+      //   const newString = obj.hash;
+      //   let num = null;
+      //   for (let i = newString.length - 1; i >= 0; i--) {
+      //     if (!isNaN(parseInt(newString[i]))) {
+      //       num = parseInt(newString[i]);
+      //       break;
+      //     }
+      //   }
+      //   fd.append("slotid", num);
+      //   fd.append("overall", JSON.stringify(obj));
+      //   //  trx 1
+      //   try {
+      //      if (String(isAlreadyHit) === String(prevalue)) return;
+      //     const response = await axios.post(
+      //       "https://admin.zupeeter.com/Apitrx/insert_one_trx",
+      //       fd
+      //     );
+      //         isAlreadyHit = prevalue;
 
-        } catch (e) {
-          console.log(e);
-        }
-      }
+      //   } catch (e) {
+      //     console.log(e);
+      //   }
+      // }
     }, [6000]);
   } catch (e) {
     console.log(e);
